aadee3e4ba741182b8dba3703872962a83d6e645
Mikhail Volkhov <volhovm.cs@gmail.com>
2016-09-12 21:37:33 +0300

    Done hw1

--- Homework1.hs
+++ Homework1.hs
@@ -1,4 +1,62 @@
-module Main where
+module Main (main) where
+
+import           Control.Lens
+import           Control.Monad  (forM_, unless)
+import           Data.Bifunctor (bimap, first)
+import           Data.Char      (isDigit)
+import           Data.Maybe     (catMaybes, isJust)
+import           System.Random  (newStdGen, randomRs)
+
+stringSum :: String -> Int
+stringSum "" = 0
+stringSum s = loop 0 s
+  where
+    parseInt :: String -> (Int,String)
+    parseInt = head . reads
+    nextChunk = dropWhile (\c -> not $ isDigit c || c `elem` "+-")
+    loop acc s' =
+        let cont = uncurry loop . first (+acc) . parseInt in
+        case nextChunk s' of
+            []       -> acc
+            ('+':xs) -> cont xs
+            s''      -> cont s''
+
+zipN :: ([a] -> b) -> [[a]] -> [b]
+zipN foo input =
+    catMaybes $
+    takeWhile isJust $
+    map (\i -> case input ^.. (each . ix i) of
+                   [] -> Nothing
+                   a  -> Just $ foo a)
+        [0..]
+
+mergeSort :: [Int] -> [Int]
+mergeSort a | length a < 2 = a
+mergeSort l =
+    uncurry merge $
+    bimap mergeSort mergeSort $
+    splitAt (length l `div` 2) l
+  where
+    merge a []                = a
+    merge [] b                = b
+    merge l1@(a:xs) l2@(b:ys) =
+        if a < b then a : merge xs l2 else b : merge l1 ys
+
+randomIntList :: Int -> Int -> Int -> IO [Int]
+randomIntList n f t = take n . randomRs (f,t) <$> newStdGen
+
+testMerge :: IO ()
+testMerge =
+    forM_ [0..100] $ \i -> do
+        l <- randomIntList i 0 50
+        let sorted = mergeSort l
+        unless (predicate sorted) $
+            error $ "Failed on:\n" ++ show l ++ "\n" ++ show sorted
+  where
+    predicate []  = True
+    predicate [_] = True
+    predicate l   =
+        all (\i -> (l !! i) <= (l !! (i + 1))) [0..(length l - 2)]
 
 main :: IO ()
-main = main
+main = testMerge
