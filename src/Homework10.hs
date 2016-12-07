0d9ac89c6197a1f11a0c932adc0d6539bc122051
Mikhail Volkhov <volhovm.cs@gmail.com>
2016-12-07 20:31:35 +0300

    Add 7-8.1,2

--- src/Homework10.hs
+++ src/Homework10.hs
@@ -11,7 +11,7 @@ module Homework10 where
 
 import qualified Base
 import           Control.DeepSeq        (deepseq, ($!!))
-import           Control.Lens           (makeLenses, makePrisms)
+import           Control.Lens           hiding ((&))
 import           Control.Monad.IO.Class (liftIO)
 import           Data.Functor.Const     (Const (..))
 import           Data.Functor.Identity  (Identity (..))
@@ -23,7 +23,8 @@ import           Language.Haskell.TH    (Body (..), Clause (..), Con (..), Dec (
                                          reify, varE)
 import           System.Directory       (doesDirectoryExist, doesFileExist, listDirectory)
 import           System.Environment     (lookupEnv)
-import           System.FilePath        (splitPath, takeFileName, (</>))
+import           System.FilePath        (replaceExtension, splitPath, takeExtension,
+                                         takeFileName, (</>))
 import           System.IO.Unsafe       (unsafePerformIO)
 import           Universum
 
@@ -142,24 +143,24 @@ fibonacciTH n = [|(fib n :: Integer)|]
 -- Task 5
 ----------------------------------------------------------------------------
 
-type Lens s t a b = forall f . Functor f => (a -> f b) -> s -> f t
-type Simple f s a = f s s a a
-type Lens' s a = Simple Lens s a
+type MLens s t a b = forall f . Functor f => (a -> f b) -> s -> f t
+type MSimple f s a = f s s a a
+type MLens' s a = MSimple MLens s a
 
-type Getting r s a = (a -> Const r a) -> s -> Const r s
-type Setting s t a b = (a -> Identity b) -> s -> Identity t
+type MGetting r s a = (a -> Const r a) -> s -> Const r s
+type MSetting s t a b = (a -> Identity b) -> s -> Identity t
 
 -- | Make a lens out of a getter and a setter.
-lens :: (s -> a) -> (s -> b -> t) -> Lens s t a b
+lens :: (s -> a) -> (s -> b -> t) -> MLens s t a b
 lens get set f s = set s <$> f (get s)
 
-set' :: Setting s t a b -> b -> s -> t
+set' :: MSetting s t a b -> b -> s -> t
 set' l new = runIdentity . l (Identity . const new)
 
-view' :: Getting a s a -> s -> a
+view' :: MGetting a s a -> s -> a
 view' l = getConst . l (\x -> Const x)
 
-over' :: Setting s t a b -> (a -> b) -> s -> t
+over' :: MSetting s t a b -> (a -> b) -> s -> t
 over' l c = runIdentity . l (Identity . c)
 
 -- (.~) = set
@@ -172,13 +173,26 @@ over' l c = runIdentity . l (Identity . c)
 
 -- | VFS representation
 data FS
-    = Dir { _name     :: FilePath
+    = Dir { _fname    :: FilePath
           , _contents :: [FS]}
-    | File { _name :: FilePath}
-    deriving (Show)
+    | File { _fname :: FilePath}
+
+instance Show FS where
+    show (File n) = n
+    show o@(Dir n contents) = show' 0 o
+      where
+        spaces' n = replicate n ' '
+        show' n (File a) = spaces' n ++ a
+        show' n (Dir a c) =
+            concat [spaces' n, a, " ->", concatMap (("\n" ++) . show' (n + 2)) c]
 
 makeLenses ''FS
 makePrisms ''FS
+
+isFile, isDir :: FS -> Bool
+isFile (File _)  = True
+isFile (Dir _ _) = False
+isDir = not . isFile
 
 retrieveFS ::  FilePath -> IO FS
 retrieveFS fs = do
@@ -213,4 +227,132 @@ Nothing
 λ> k ^? _Dir
 Just ("src",[File {_name = "Homework3.hs"},File {_name = "Fib.hs"},File {_name = "Setup.hs"},File {_name = "Homework10.hs"},File {_name = "BTree.hs"},File {_name = "Homework2.hs"},Dir {_name = "kek", _contents = [File {_name = "mda"}]},File {_name = "Main.hs"},File {_name = "Homework10Exe.hs"},File {_name = "Homework1.hs"}])
 
+-}
+
+-- Filename -> (FS -> f FS) -> FS -> ???
+-- λ> :t lens
+-- lens
+--   :: Functor f => (s -> a) -> (s -> b -> t) -> (a -> f b) -> s -> f t
+-- λ> :t prism
+-- prism
+--   :: (Choice p, Applicative f) =>
+--      (b -> t) -> (s -> Either t a) -> p a (f b) -> p s (f t)
+
+-- | Lens on every subfolder that matches (== dirname)
+cd :: FilePath -> Traversal' FS FS
+cd _ _ o@(File _) = pure o
+cd dirname foo (Dir name contents) =
+    let matched = filter dirMatches contents
+        others = filter (not . dirMatches) contents
+    in Dir name . (others ++) <$> traverse foo matched
+  where
+    dirMatches (File _)  = False
+    dirMatches (Dir n _) = n == dirname -- insert '*' matching here
+
+{-
+λ> t <- retrieveFS "."
+λ> t ^.. cd "src" . cd "kek"
+[Dir {_name = "kek", _contents = []}]
+λ> t ^.. cd "src" . ls . fname
+["Homework3.hs","Fib.hs","Setup.hs","Homework10.hs","BTree.hs",
+"Homework2.hs","kek","Main.hs","Homework10Exe.hs","Homework1.hs"]
+λ> t ^.. cd "src" .  file "Setup.hs"
+[File {_fname = "Setup.hs"}]
+λ> t ^.. cd "src" .  file "Setaoeu"
+[]
+-}
+
+-- | Lens on directory contents (same as `contents`, almost)
+ls :: Traversal' FS FS
+ls _ o@(File _)            = pure o
+ls foo (Dir name contents) = Dir name <$> traverse foo contents
+
+-- | Lens on every file in dir that matches (== filename)
+file :: FilePath -> Traversal' FS FS
+file _ _ o@(File _) = pure o
+file filename foo (Dir name contents) =
+    Dir name <$> traverse foo (filter dirMatches contents)
+  where
+    dirMatches (File n) = n == filename
+    dirMatches _        = False
+
+----------------------------------------------------------------------------
+-- Task 8
+----------------------------------------------------------------------------
+
+files :: Traversal' FS FS
+files _ o@(File _)     = pure o
+files foo (Dir name c) =
+    Dir name . (filter isDir c ++) <$> traverse foo (filter isFile c)
+
+changeExtension :: [Char] -> FS -> FS
+changeExtension newExt = files . fname %~ flip replaceExtension newExt
+
+{-
+λ> t ^?! cd "src"
+src ->
+  Homework3.hs
+  Fib.hs
+  Setup.hs
+  Homework10.hs
+  BTree.hs
+  Homework2.hs
+  kek ->
+  Main.hs
+  Homework10Exe.hs
+  Homework1.hs
+λ> changeExtension "kek" $ t ^?! cd "src"
+src ->
+  kek ->
+  Homework3.kek
+  Fib.kek
+  Setup.kek
+  Homework10.kek
+  BTree.kek
+  Homework2.kek
+  Main.kek
+  Homework10Exe.kek
+  Homework1.kek
+-}
+
+-- | Traversal of all subnames
+fnameRec :: Traversal' FS FilePath
+fnameRec = getNamesRec False
+  where
+    getNamesRec :: Bool -> Traversal' FS FilePath
+    getNamesRec _ _ o@(File _) = pure o
+    getNamesRec incl foo (Dir name contents) =
+        let changedFiles = traverse (fname foo) $ filter isFile contents
+            changedDirs =
+                traverse (getNamesRec True foo) $ filter isDir contents
+            combined = (++) <$> changedFiles <*> changedDirs
+        in (if incl
+            then (<*>) (Dir <$> foo name)
+            else fmap (Dir name)) combined
+
+{-
+λ> t & cd "src" . getNamesRec %~ (++ ".LOL")
+... lots of trash
+      exclude
+    index
+    refs ->
+      heads ->
+        master
+      remotes ->
+        origin ->
+          master
+      tags ->
+    HEAD
+  .gitignore
+  src ->
+    Homework3.hs.LOL
+    Fib.hs.LOL
+    Setup.hs.LOL
+    Homework10.hs.LOL
+    BTree.hs.LOL
+    Homework2.hs.LOL
+    Main.hs.LOL
+    Homework10Exe.hs.LOL
+    Homework1.hs.LOL
+    kek.LOL ->
 -}
